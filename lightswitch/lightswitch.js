ac242a97d8b513c108b7405e5931c33e32e01f6e
forsgren8 <36867090+forsgren8@users.noreply.github.com>
2018-03-18 11:25:48 -0400

    adding Js

--- lightswitch/lightswitch.js
+++ lightswitch/lightswitch.js
@@ -1,1 +1,12 @@
+function start() {
+  var click = document.getElementById("slider");
+  click.addEventListener("click", toggle);
+};
 
+function toggle() {
+  var color = document.getElementById("body");
+  var backColor = color.style.backgroundColor;
+  color.style.backgroundColor = backColor === "black" ? "white" : "black";
+};
+
+start();
