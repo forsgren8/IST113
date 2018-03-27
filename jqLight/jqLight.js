9b1c5cac8686fbc2a127088e8d155f230d01256f
forsgren8 <36867090+forsgren8@users.noreply.github.com>
2018-03-26 20:31:05 -0400

    complete!

--- jqLight/jqLight.js
+++ jqLight/jqLight.js
@@ -1,1 +1,13 @@
-
+$(document).ready(function(){
+	$("#slider").on("click", function(event){toggle();
+	});
+});
+function toggle(){
+	console.log("toggle");
+if($("input").prop("checked")){
+		$("body").css("background-color", "black");
+	  }
+	  else{
+		  $("body").css("background-color", "white");
+	  }
+} 
